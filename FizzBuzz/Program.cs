a6bd09b94886e38f1abb255883862010f04c53f3
Jonah <jghebremichael95@gmail.com>
2023-01-17 17:49:20 -0500

    initial

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(FB(3));
+
+
+string FB(int num)
+{
+    string s = "";
+    if (num % 3 == 0) { s += "fizz"; }
+    if (num % 5 == 0) { s += "buzz"; }
+
+    return s;
+}
